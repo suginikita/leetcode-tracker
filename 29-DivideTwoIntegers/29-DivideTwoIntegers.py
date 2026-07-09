04598e0e03aa64e73a0e50ea314a2a965ffc3ffc
suginikita T R <suginikita@gmail.com>
2026-07-09 10:06:44 +0530

    Create 29-DivideTwoIntegers

---
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.py
@@ -0,0 +1,22 @@
+# Last updated: 7/9/2026, 10:06:42 AM
+class Solution:
+    def divide(self, dividend, divisor):
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+        if dividend == INT_MIN and divisor == -1:
+            return INT_MAX
+        negative = (dividend < 0) != (divisor < 0)
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+        quotient = 0
+        while dividend >= divisor:
+            temp = divisor
+            multiple = 1
+            while dividend >= (temp << 1):
+                temp <<= 1
+                multiple <<= 1
+            dividend -= temp
+            quotient += multiple
+        if negative:
+            quotient = -quotient
+        return quotient
