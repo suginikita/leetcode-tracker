acc1042d74c5bece7d1064358ce494a01be948b9
suginikita T R <suginikita@gmail.com>
2026-07-09 10:06:21 +0530

    Create 180-ConsecutiveNumbers

---
+++ 180-ConsecutiveNumbers/180-ConsecutiveNumbers.sql
@@ -0,0 +1,7 @@
+-- Last updated: 7/9/2026, 10:06:19 AM
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 ON l1.id = l2.id - 1
+JOIN Logs l3 ON l1.id = l3.id - 2
+WHERE l1.num = l2.num
+  AND l2.num = l3.num;
