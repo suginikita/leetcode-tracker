539c480e05bf0a6fa90a269167c02fc18741f2fa
Suginikita T R <suginikita@gmail.com>
2026-08-22 09:20:59 +0530

    Create 86-PartitionList

---
+++ 86-PartitionList/version/java/86-PartitionList_20260822_092102.java
@@ -0,0 +1,23 @@
+// Last updated: 8/22/2026, 9:21:02 AM
+1class Solution {
+2    public int numTrees(int n) {
+3        
+4        int[] dp = new int[n + 1];
+5        
+6        dp[0] = 1;
+7        dp[1] = 1;
+8        
+9        for (int nodes = 2; nodes <= n; nodes++) {
+10            
+11            for (int root = 1; root <= nodes; root++) {
+12                
+13                int left = root - 1;
+14                int right = nodes - root;
+15                
+16                dp[nodes] += dp[left] * dp[right];
+17            }
+18        }
+19        
+20        return dp[n];
+21    }
+22}
